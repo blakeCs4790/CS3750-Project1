5eec78e73d326807cb78d5dab0a33dea46430313
blakeCs4790 <freemanblake@mail.weber.edu>
2017-01-20 17:57:59 -0700

    Example
    I was messing around at work and decided to put together something similar to our project. I used a table from work so it is not exactly what we are doing, but it might help with the front end.  I was able to show the database table in a grid and was able to add the edit, delete, and add buttons.  I was also able to pass the table id's down to javascript functions.  I did not have time to figure out how to pop a pop up window for the edit and add functions.  It might be useful if you run into any problems with the front end.

---
+++ HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public List<IssueList> GetIssues()
+        {
+            List<IssueList> list = new List<IssueList>();
+            using (var db = new MvcAuctionEntities())
+            {
+                var issues = (from r in db.NinissueAdmin
+                              select r);
+                foreach (var e in issues)
+                {
+                    list.Add(new IssueList { IssueId = e.IssueId, Issue = e.Issue, Category = e.Category });
+                }
+            }
+            return list;
+        }
+
+        [HttpPost]
+        public void Edit(int id)
+        {
+
+        }
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+
+        }
+
+        [HttpPost]
+        public void Add()
+        {
+
+        }
+
+    }
+}
---
+++ IssuesAdminModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Models
+{
+    public class IssuesAdminModel
+    {
+        public List<IssueList> IssuesCollection { get; set; }
+        public IssueList Issues { get; set; }
+    }
+
+    public class IssueList
+    {
+        public int IssueId { get; set; }
+        public string Issue { get; set; }
+        public string Category { get; set; }
+    }
+}
